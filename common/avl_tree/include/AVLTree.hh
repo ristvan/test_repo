96ec1183b9d802d51ba3d877fd847122cc4e072a
Istvan Rejto <reistvan@gmail.com>
2015-05-08 22:28:49 +0200

    [common] import AVL Tree implementation

---
+++ common/avl_tree/include/AVLTree.hh
@@ -0,0 +1,67 @@
+#ifndef __AVLTREE_H
+#define __AVLTREE_H
+
+
+enum TTypeOfRotation
+  {
+  SimpleLeftRotation,
+  SimpleRightRotation,
+  DoubleLeftRotation,
+  DoubleRoghtRotation
+  };
+
+enum TTypeOfModification
+  {
+  AddNewItem,
+  DeleteItem
+  };
+
+class AVLTreeNodeItem;
+
+class AVLTree
+  {
+private:
+
+  class AVLTreeNode
+    {
+  public:
+    AVLTreeNodeItem *iItem;
+    AVLTreeNode *iDad;
+    AVLTreeNode *iLeft;
+    AVLTreeNode *iRight;
+    
+    void Destroy(); // Destroy Every Item under this Node
+    AVLTreeNode();
+    ~AVLTreeNode();
+    void PreOrder();
+    void InOrder();
+    void PostOrder();
+//    AVLTreeNodeItem* InOrder(const int aIndex);
+    char iBalance; 
+    int iRelativePos; // in AVL array-like type   
+    };
+    
+  AVLTreeNode *iRoot;
+  int iNumberOfElements;
+  
+public:
+  AVLTree();
+  ~AVLTree();
+  bool Add(AVLTreeNodeItem& aNodeItem);
+  bool Remove(AVLTreeNodeItem& aNodeItem);
+  void RemoveAll();
+  int Count(void) const { return iNumberOfElements; }
+  void PreOrder();
+  void InOrder();
+  void PostOrder();
+  AVLTreeNodeItem& At(const int iIndex); 
+  AVLTreeNodeItem& At(const int iIndex) const; 
+  AVLTreeNodeItem& operator[](const int iIndex); 
+  AVLTreeNodeItem& operator[](const int iIndex) const; 
+
+private:
+  void Corrector(AVLTreeNode *aNode, bool right, TTypeOfModification aMod);
+//  int NewBalance(TTypeOfRotation aRotation, TTypeOfNode aNode, int aOldBalance);
+  };
+
+#endif //__AVLTREE_H
---
+++ common/avl_tree/include/AVLTreeNodeItem.hh
@@ -0,0 +1,41 @@
+#ifndef __AVLTREENODEITEM_H
+#define __AVLTREENODEITEM_H
+
+class AVLTreeNodeItem
+  {
+private:
+  int i;
+public:
+  AVLTreeNodeItem(int aI)
+    {
+    i = aI;
+    }
+    
+  bool operator==(const AVLTreeNodeItem& aOther)
+    {
+      return i == aOther.i;
+    }
+
+  bool operator!=(const AVLTreeNodeItem& aOther)
+    {
+      return i != aOther.i;
+    }
+
+     
+  bool operator<(const AVLTreeNodeItem& aOther)
+    {
+      return i < aOther.i;
+    }
+
+  bool operator>(const AVLTreeNodeItem& aOther)
+    {
+      return i > aOther.i;
+    }
+  
+  void Printf()
+    {
+    printf("%d,",i); fflush(stdout);
+    }
+  };
+
+#endif //__AVLTREENODEITEM_H
