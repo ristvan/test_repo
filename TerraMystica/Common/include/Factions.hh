9f76c2746f73f81a3f3fabf8f1dca7df992f2b58
Istvan Rejto <reistvan@gmail.com>
2015-05-29 20:44:51 +0200

    [TerraMystica] Add Common::Factions for list all the Current Factions.

---
+++ TerraMystica/Common/include/Factions.hh
@@ -0,0 +1,19 @@
+#pragma once
+
+enum Factions
+{
+    eFakirs,
+    eNomads,
+    eChaosMagicians,
+    eGiants,
+    eSwarmlings,
+    eMermaid,
+    eDwarves,
+    eEngineers,
+    eHalflings,
+    eCultists,
+    eAlchemists,
+    eDarklings,
+    eAuren,
+    eWitches
+};
